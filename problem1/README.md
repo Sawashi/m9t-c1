f20930087b6db834f1096355320b8d624ed14986
Kiet <92447748+Sawashi@users.noreply.github.com>
2025-01-07 19:21:54 +0700

    Init problem 1

---
+++ problem1/README.md
@@ -0,0 +1,1 @@
+The assignment not say n is positive so i have to check it
---
+++ problem1/script.js
@@ -0,0 +1,36 @@
+//Please read the attached README.md for my notes
+
+var sum_to_n_a = function (n) {
+	// your code here
+	if (n < 0) {
+		return (n * (n - 1)) / -2;
+	}
+	return (n * (n + 1)) / 2;
+};
+
+var sum_to_n_b = function (n) {
+	// your code here
+	if (n == 0) {
+		return 0;
+	}
+	if (n < 0) {
+		return n + sum_to_n_b(n + 1);
+	} else {
+		return n + sum_to_n_b(n - 1);
+	}
+};
+
+var sum_to_n_c = function (n) {
+	// your code here
+	let adjustNum = n < 0 ? -1 : 1;
+	let sum = 0;
+	for (let i = 0; i <= Math.abs(n); i++) {
+		sum += i * adjustNum;
+	}
+	return sum;
+};
+function runAll(n) {
+	console.log(sum_to_n_a(n));
+	console.log(sum_to_n_b(n));
+	console.log(sum_to_n_c(n));
+}
